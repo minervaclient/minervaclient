39a083b8397f2a4a163413f9d5155e32e9f6d2df
Ryan Au <auryan898@gmail.com>
2018-06-23 08:36:00 -0400

    Updated the versioning and metadata when packaged for pypi

--- minervaclient/__init__.py
+++ minervaclient/__init__.py
@@ -1,4 +1,42 @@
+__version__ = '0.0.0'
+""" September 1, 2017 (nicholaspaun)
+Notes:
+1.  * You can now view your final exam schedule (`minervac sched -E`)
+2.  * You can now query your transcript (`minervac transcript`)
+3.  * A visual timetable feature has been implemented. (-V).
+    * Calendar export is now available (vCalendar format only). (-C)
+4.  * The command-line interface won't be modified anymore, only extended. Configuration format is still in flux.
+    * Displaying course schedule information with custom reports.
+
+"""
+
 __version__ = '1.0.0'
-"""First Version of minervaclient package"""
+""" June 20, 2018
+New Features:
+
+- Converted original project by [nicholaspaun](https://github.com/nicholaspaun/minervaclient/) into 
+an installable pip package that will be added to the PyPi system for easier installation
+
+- Added support for automatically installing the dependencies for this project such as `requests`, 
+`beautifulsoup4`, and `urllib5`
+
+- Added an explicit feature, `minervac search` in the command line app, for querying Minerva for 
+course information on CRNs, instructors, times, dates, waitlists, and general availability
+
+- tTe only command that works now is `minervac` which should replace the previous iteration's `mnvc` 
+and `minervaclient`
+
+- Updated the installation methods, but the majority of features only work if installing via build 
+from source.  See [note](https://github.com/auryan898/minervaclient#install-via-pip)
+
+"""
+
+__version__ = '1.1.0'
+""" Patch 1.1.0 - June 22, 2018
+New Feature:
+- Now any form of installation has the same functionality
+- Implement an on-screen login when running features that require Minerva login
+
+"""
 
 __all__ = ['reg','sched','exams','transcript','minerva_common','config','pub_search']
--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(name='minervaclient',
-      version='1.0.0',
-      description='Tools and Utilities that can be used to access McGill University\'s Minerva service',
-      author='Ryan B Au',
-      author_email='auryan898@gmail.com',
-      url='https://github.com/auryan898/minervaclient',
-      # download_url='https://github.com/auryan898/nxt-python-tools/archive/0.1.tar.gz',
-      keywords=['minerva','banner','mcgill','college','schedule'],
-      packages=['minervaclient'],
-      scripts=['scripts/minervac'],
-      classifiers=[],
-      install_requires=[
-          'requests', 'beautifulsoup4', 'html5lib'
-      ],
-      license='MIT'
+    version='1.1.0',
+    description='Tools and Utilities that can be used to access McGill University\'s Minerva service',
+    author='Ryan B Au',
+    author_email='auryan898@gmail.com',
+    url='https://github.com/auryan898/minervaclient',
+    download_url='https://github.com/auryan898/minervaclient/archive/master.zip',
+    keywords=['minerva','banner','mcgill','college','schedule'],
+    packages=['minervaclient'],
+    scripts=['scripts/minervac'],
+    classifiers=[],
+    install_requires=[
+        'requests', 'beautifulsoup4', 'html5lib'
+    ],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
      )
